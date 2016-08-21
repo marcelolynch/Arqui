8a88682f6e73aaaa80bdc8aead4e1101a661bb16
Chelo <mlynch@itba.edu.ar>
2016-08-21 17:06:05 -0300

    El ejercicio 12 100% lindo

--- TP2/Ejercicio12/ejercicio12.asm
+++ TP2/Ejercicio12/ejercicio12.asm
@@ -6,7 +6,7 @@ EXTERN print
 section .text
 _start:
 
-	;El stack se organiza:
+	;El stack se organiza asi al inicio de la llamada:
 	;
 	;----------------------------
 	; ESP          |   Cantidad de args
@@ -30,6 +30,10 @@ _start:
 	; NULL (4 bytes)
 	;---------------------------
 	;
+	; Este programa recorre el stack de a 4 bytes, acumulando la cantidad de bytes
+	; recorridos en EAX e imprimiendo los contenidos del stack por salida estandar
+	; Primero se imprime la cantidad de argumentos, luego los argumentos y finalmente 
+	; las variables de entorno.
 	; El ciclo interno arranca con ESI en ESP + 8 (Path) 
 	; y corta cuando encuentra un NULL (no arranca en ESP para no )
 	; El externo luego de dos iteraciones (ECX empieza en 2)
@@ -54,9 +58,34 @@ _start:
 	call numtos
 	call print
 
+	add esi, 4		;Me muevo en ESP + 4 para imprimir todo el resto: en ESP no hay un puntero sino un numero
+	mov eax, 4 		;Ya recorri 4 bytes en el stack
+
+	;ARGUMENTOS DEL PROGRAMA (se imprime el path y cualquier otro argumento)
+
+	;Muesto el mensaje:
 	mov ebx, sep
 	call print
 
+	mov ebx, newline
+	call print
+
+	;Imprimo mensaje
+	mov ebx, sep
+	call print
+
+	mov ebx, msg0
+	call print
+		
+	mov ebx, sep
+	call print
+
+
+	call print_til_null ;Imprimo los contenidos del stack hasta encontrar un null
+
+
+	;VARIABLES DE ENTORNO
+	;Primero imprimo mensaje
 	mov ebx, newline
 	call print
 
@@ -71,22 +100,7 @@ _start:
 	call print
 
 
-
-	add esi, 4		;Arranco en ESP + 4 para imprimir todo el resto: en ESP no hay un puntero sino un numero
-	mov eax, 4 		;Ya recorri 4 bytes en el stack
-
-	mov ecx, 2 ;Dos iteraciones
-	.til_null:
-		.lp:
-			mov ebx, [esi]
-			call print_it	;Se imprime
-			
-			add esi, 4		;Avanzo en el stack
-			add eax, 4		;En EAX acumulo cuantos bytes recorro
-
-		test ebx, ebx
-		jnz .lp 		;Hasta que en ebx haya un null
-	loop .til_null	
+	call print_til_null
 
 
 	;Imprimo mensaje final (cantidad de bytes)
@@ -105,6 +119,23 @@ _start:
 
 	call exit
 
+
+
+
+print_til_null:
+	.loop:
+			mov ebx, [esi]
+			call print_it	;Se imprime
+			
+			add esi, 4		;Avanzo en el stack
+			add eax, 4		;En EAX acumulo cuantos bytes recorro
+
+		test ebx, ebx
+	jnz .loop	;Hasta que en ebx haya un null
+ret
+
+
+
 print_it:
 	push ebx
 	test ebx, ebx
@@ -120,7 +151,8 @@ print_it:
 section .rodata
 	newline db 10, 0
 	sep db 10, "    ======================================", 10, 0
-	msg1 db "            VARIABLES DE ENTORNO", 0
+	msg0 db "            PATH y ARGUMENTOS", 0
+	msg1 db "         VARIABLES DE ENTORNO", 0
 	msg2 db "     CANTIDAD DE BYTES EN EL STACK: ",0
 	msg_args db "        CANTIDAD DE ARGUMENTOS: ",0
 
